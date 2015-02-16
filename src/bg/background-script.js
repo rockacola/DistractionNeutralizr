7f9f475d6af1580e2f7780b70954726c91ca2418
Travis <travis@travislin.com>
2015-02-16 11:24:46 +1100

    Update to seek for background-image attribute on all tags and apply implementation to elements matches the  hardcoded criteria

--- src/bg/background-script.js
+++ src/bg/background-script.js
@@ -1,38 +1,79 @@
 //-- Constants
 var ACTIVE_OPACITY = 0.05; // TODO: Setting value
 var FADING_SPEED = 100; // TODO: Setting value
+var BG_IMAGE_MAX_WIDTH = 300;
+var BG_IMAGE_MAX_HEIGHT = 300;
+//var verbose = true; //TODO: Setting to enable console print
+//var hoverOnDelay = 500; //TODO: Setting for delaying display of hover on on concealed elements (in ms)
 
-
-
-//-- Hunt <img> & <iframe> tags on page
+//-- Seeks <img> & <iframe> tags on page
 $('img, iframe').each(function() {
-    //console.log('opacity of this elem:', $(this).css('opacity'));
-    $(this).addClass('dn-img-active')
-           .data('dn-default-opacity', $(this).css('opacity'))
-           .css({ opacity: ACTIVE_OPACITY });
+    concealGenericElement($(this));
 });
 
-//-- Hunt <a> tags with background-image attribute on page
-$('a').each(function() { //TODO: I want to query all elements
-    if ($(this).css('background-image') != 'none') {
-        //console.log('There is a background image:', $(this).css('background-image'));
-        $(this).addClass('dn-img-active')
-               .data('dn-default-opacity', $(this).css('opacity'))
-               .css({ opacity: ACTIVE_OPACITY });
+//-- Seeks for background-image attribute on all tags
+$('*').each(function() { //TODO: Can I make this query more efficient?
+    if ($(this).css('background-image') != 'none') { // Only concern about elements that has background images
+        //console.log('Tag:', $(this).prop('tagName'),
+        //            'BgRepeat:', $(this).css('background-repeat'),
+        //            'BgImg:', $(this).css('background-image'));
+        if($(this).css('background-repeat') == 'no-repeat') { // Only concern about elements that has none repeating background
+            var imageObj = generateImageObject($(this));
+            if (typeof imageObj != "undefined") {
+                //console.log('imageObj', imageObj, 'width:', imageObj.width, 'height:', imageObj.height);
+                if(imageObj.width > BG_IMAGE_MAX_WIDTH && imageObj.height > BG_IMAGE_MAX_HEIGHT) {
+                    concealBackgroundImageElement($(this));
+                }
+            }
+        }
     }
 });
-
-
 
 
 
 //-- Event Binding
 $('body').on('mouseenter', '.dn-img-active', function() {
     var defaultOpacity = ($(this).data('dn-default-opacity') != '') ? $(this).data('dn-default-opacity') : 1;
-    //console.log('mouseenter! defaultOpacity:', defaultOpacity);
     $(this).fadeTo(FADING_SPEED, defaultOpacity);
 });
 $('body').on('mouseleave', '.dn-img-active', function() {
-    //console.log('mouseleave!');
     $(this).fadeTo(FADING_SPEED, ACTIVE_OPACITY);
 });
+
+
+
+//-- Methods
+
+// Create an Image object by finding the 'background-image' of an jQuery selector element
+// REF: http://stackoverflow.com/a/12784180/174774
+function generateImageObject(targetElem) {
+    var imageUrl = targetElem.css('background-image');
+    var imageObj;
+    //console.log('imageObj', imageObj);
+    imageUrl = imageUrl.match(/^url\("?(.+?)"?\)$/); // Remove url() or in case of Chrome url("")
+
+    if (imageUrl[1]) {
+        imageUrl = imageUrl[1];
+        imageObj = new Image();
+
+        //// just in case it is not already loaded
+        //$(imageObj).load(function () {
+        //    //alert(imageObj.width + 'x' + imageObj.height);
+        //});
+
+        imageObj.src = imageUrl;
+    }
+
+    return imageObj;
+}
+
+function concealGenericElement(targetElem) {
+    return targetElem.addClass('dn-img-active')
+                     .data('dn-default-opacity', targetElem.css('opacity'))
+                     .css({ opacity: ACTIVE_OPACITY });
+}
+
+function concealBackgroundImageElement(targetElem) {
+    return targetElem.data('dn-default-background-image', targetElem.css('background-image'))
+                     .css('background-image', 'none');
+}
