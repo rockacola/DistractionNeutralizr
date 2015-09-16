ea9795b1ef5f65072f64f388be1f551880198475
Travis <travis@travislin.com>
2015-09-16 12:30:50 +1000

    Retain image's original width and height. Add bit of transparency to better blend with its background. Add support for identifying 'ad elements'.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -23,6 +23,7 @@ var TheInstance = window.App = window.App || {
     //TODO: feature to allow toggle display of content with click
 
     init: function() {
+        //TODO: What happen when this is triggered multiple times?
         log('Initialise Distraction Neutralizr. isDebug:', this.isDebug);
 
         this._performIntervalCheck(); // perform the 1st evaluation
@@ -39,6 +40,10 @@ var TheInstance = window.App = window.App || {
             _this._muteImgElement($img);
         });
 
+        var adElementCollection = window.document.querySelectorAll('.ad:not(.dn-flag), .adContainer:not(.dn-flag)');
+        Utils.forEach(adElementCollection, function($ad) {
+            _this._muteAdElement($ad);
+        });
         //TODO: support Google Adword elements
         //TODO: support background image
         //TODO: support flash
@@ -51,18 +56,41 @@ var TheInstance = window.App = window.App || {
             return;
         }
 
-        //TODO: attempt to retain its original width and height
-        //TODO: add a bit of transparency to better blend with its background
-
+        // Add flags and backup original content
+        $img.classList.add('dn-flag');
         $img.setAttribute('data-dn-enabled', true);
         $img.setAttribute('data-dn-img-src', $img.src);
         $img.setAttribute('data-dn-img-alt', $img.alt);
-        $img.classList.add('dn-flag');
-        $img.style.backgroundColor = '#aaa';
+        // Override existing element attributes
+        //TODO: May consider using 'setProperty' attribute to set '!important' (REF: http://stackoverflow.com/a/463134/174774)
+        $img.style.width = $img.offsetWidth + 'px';
+        $img.style.height = $img.offsetHeight + 'px';
+        $img.style.backgroundColor = 'rgba(170, 170, 170, 0.6)';
         $img.setAttribute('src', '');
         $img.setAttribute('srcset', '');
+        $img.setAttribute('data-src', '');
         $img.setAttribute('alt', '');
     },
+
+    _muteAdElement: function($ad) {
+        log('_muteAdElement triggered.');
+
+        if(!$ad) { //TODO: Better validation check
+            return;
+        }
+
+        // Add flags
+        $ad.classList.add('dn-flag');
+        $ad.setAttribute('data-dn-enabled', true);
+        // Override existing element attributes
+        $ad.style.width = $ad.offsetWidth + 'px';
+        $ad.style.height = $ad.offsetHeight + 'px';
+        $ad.style.backgroundColor = 'rgba(170, 170, 170, 0.6)';
+
+        while($ad.lastChild) {
+            $ad.removeChild($ad.lastChild);
+        }
+    }
 };
 
 window.App = TheInstance; // use 'window.app' for easier debugging through browser console.
