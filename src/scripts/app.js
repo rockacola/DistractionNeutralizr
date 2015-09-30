7e4dfefcabdf987f7e3b2bf5c454108043843950
Travis <travis@travislin.com>
2015-09-30 16:10:29 +1000

    Simplified but better support on images and iframes.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -37,7 +37,10 @@ var Utils = require('./base/utils');
  *
  *
  * Roadmap:
+ * - Flash support
+ * - Video support
  * - Overlay icon on disabled elements and show corresponding annotation icons to effects.
+ * - Detects and hides intrusive background images
  *
  */
 
@@ -48,35 +51,27 @@ var TheInstance = window.App = window.App || {
     isDebug: true, // Currently not been utilised
 
     // Configurations
-    checkIntervalMs: 2000, //TODO: Instead of using timing, perhaps using/incorporate scrolling distance?
+    checkIntervalMs: 4000, //TODO: Instead of using timing, perhaps using/incorporate scrolling distance?
 
     init: function() {
         log('Initialise Distraction Neutralizr. isDebug:', this.isDebug);
         if(!window.document.body.classList.contains('dn-active')) { // Mechanism to avoid the script been triggered multiple time.
             window.document.body.classList.add('dn-active');
-
-
             this._performIntervalCheck(); // perform the 1st evaluation
-            setInterval(this._performIntervalCheck.bind(this), this.checkIntervalMs); // perform checks every interval
+            setInterval(this._performIntervalCheck.bind(this), this.checkIntervalMs);
         }
     },
 
     _performIntervalCheck: function() {
         this._checkImgElements();
-        //this._checkIframeElements();
-        //this._checkAdElements();
-        //this._checkFlashElements();
-        //TODO: support background image
-        //TODO: manipulate elements within iframe?
-        //TODO: ability to block all iframe contents?
+        this._checkIframeElements();
     },
 
     _checkImgElements: function() {
         var _this = this;
         var imgElementCollection = window.document.querySelectorAll('img:not(.dn-flag)');
-        //var imgElementCollection = window.document.querySelectorAll('img:not([src=""])');
         Utils.forEach(imgElementCollection, function($img) {
-            _this._muteImgElement($img);
+            _this._muteBasicElement($img, 'image');
         });
     },
 
@@ -84,60 +79,24 @@ var TheInstance = window.App = window.App || {
         var _this = this;
         var iframeElementCollection = window.document.querySelectorAll('iframe:not(.dn-flag)');
         Utils.forEach(iframeElementCollection, function($iframe) {
-            _this._muteIframeElement($iframe);
+            _this._muteBasicElement($iframe, 'iframe');
         });
     },
 
-    _checkAdElements: function() {
-        var _this = this;
-        var adTargetSelectors = [
-            '.ad:not(.dn-flag)',
-            '.adContainer:not(.dn-flag)',
-            '.GoogleActiveViewClass iframe:not(.dn-flag)',
-            '.adsbygoogle:not(.dn-flag)',
-            //'#leaderboard_ad_container:not(.dn-flag)',
-            'iframe[id^="google_ads"]', // Having trouble couple this with :not(.dn-flag)
-            'iframe[id^="ad_creative"]',
-            'iframe[id^="dmad"]',
-            'iframe[id^="creativeIframe"]',
-            'iframe[id^="aswift"]',
-            '[id^="dclk-studio-creative"]',
-            '[id^="google_companion_ad"] iframe:not(.dn-flag)',
-        ];
-        //log('huh');
-        var adTargetSelectorQueryString = adTargetSelectors.join(', ');
-        //log('adTargetSelectorQueryString', adTargetSelectorQueryString);
-        var adElementCollection = window.document.querySelectorAll(adTargetSelectorQueryString);
-        //log('adElementCollection.length', adElementCollection.length);
-        Utils.forEach(adElementCollection, function($ad) {
-            if(!$ad.classList.contains('dn-flag')) {
-                _this._muteAdElement($ad);
-            }
-        });
-    },
-
-    _checkFlashElements: function() {
-        var _this = this;
-        var flashElementCollection = window.document.querySelectorAll('object[type="application/x-shockwave-flash"]:not(.dn-flag)');
-        Utils.forEach(flashElementCollection, function($flash) {
-            _this._muteFlashElement($flash);
-        });
-    },
-
-    _muteImgElement: function($el) {
+    _muteBasicElement: function($el, objectType) {
         //TODO: perhaps I should try catch this?
-        log('_muteImgElement triggered.');
+        log('_muteBasicElement triggered.');
 
         if(!$el) { //TODO: Better validation check
             return;
         }
 
         // Add flags
-        $el.classList.add('dn-flag', 'dn-object', 'dn-image');
+        $el.classList.add('dn-flag', 'dn-object', 'dn-'+objectType);
 
         // Apply object wrapper
         var $wrapper = document.createElement('div');
-        $wrapper.classList.add('dn-object-wrapper', 'dn-image-wrapper');
+        $wrapper.classList.add('dn-object-wrapper', 'dn-'+objectType+'-wrapper');
         var $elClone = $el.cloneNode(true);
         $wrapper.appendChild($elClone);
         var $elParent = $el.parentNode;
@@ -154,83 +113,14 @@ var TheInstance = window.App = window.App || {
         $elClone.style.padding = '0';
 
         // Bind Event Listener
-        $wrapper.addEventListener('click', this._imgWrapperClickHandler.bind($wrapper)); // Just in case, binding the image wrapper
-    },
-
-    _muteIframeElement: function($iframe) {
-        //TODO: this (along with its css) is so far the same as img treatment. May consolidate.
-
-        log('_muteIframeElement triggered.');
-
-        if(!$iframe) { //TODO: Better validation check
-            return;
-        }
-
-        // Add flags
-        $iframe.classList.add('dn-flag');
-        $iframe.classList.add('dn-iframe');
-
-        // Little stretch to apply a wrapper DIV around the element
-        var $iframeWrapper = document.createElement('div');
-        $iframeWrapper.classList.add('dn-iframe-wrapper');
-        $iframeWrapper.appendChild($iframe.cloneNode(true));
-        var $iframeParent = $iframe.parentNode;
-        $iframeParent.removeChild($iframe);
-        $iframeParent.appendChild($iframeWrapper);
-    },
-
-    _muteAdElement: function($ad) {
-        log('_muteAdElement triggered.');
-
-        if(!$ad) { //TODO: Better validation check
-            return;
-        }
-
-        // Add flags
-        $ad.classList.add('dn-flag');
-        $ad.setAttribute('data-dn-enabled', true);
-        // Override existing element attributes
-        $ad.style.width = $ad.offsetWidth + 'px';
-        $ad.style.height = $ad.offsetHeight + 'px';
-        $ad.style.backgroundColor = 'rgba(170, 170, 170, 0.6)';
-
-        if($ad.nodeName === 'IFRAME') {
-            $ad.src = 'about:blank';
-        } else {
-            while($ad.lastChild) {
-                $ad.removeChild($ad.lastChild);
-            }
-        }
-    },
-
-    _muteFlashElement: function($flash) {
-        log('_muteFlashElement triggered.');
-
-        if(!$flash) { //TODO: Better validation check
-            return;
-        }
-
-        // Add flags and backup original content
-        $flash.classList.add('dn-flag');
-        $flash.setAttribute('data-dn-enabled', true);
-        $flash.setAttribute('data-dn-object-data', $flash.getAttribute('data'));
-        // Override existing element attributes
-        //TODO: May consider using 'setProperty' attribute to set '!important' (REF: http://stackoverflow.com/a/463134/174774)
-        $flash.style.width = $flash.offsetWidth + 'px';
-        $flash.style.height = $flash.offsetHeight + 'px';
-        $flash.style.backgroundColor = 'rgba(170, 170, 170, 0.6)';
-        $flash.setAttribute('data', '');
-
-        while($flash.lastChild) {
-            $flash.removeChild($flash.lastChild);
-        }
+        $wrapper.addEventListener('click', this._basicWrapperClickHandler.bind($wrapper)); // Just in case, binding the image wrapper
     },
 
     // Event Bindings
 
-    _imgWrapperClickHandler: function(e) {
+    _basicWrapperClickHandler: function(e) {
         if(!this.classList.contains('dn-disabled')) {
-            log('_imgWrapperClickHandler triggered');
+            log('_basicWrapperClickHandler triggered');
             e.preventDefault(); // NOTE: This will also disable original's anchor behaviour.
             this.classList.add('dn-disabled');
         }
